51fbbcd00edf01c8591ec307e42436f97d47e277
YF <1572843061@qq.com>
2023-05-24 17:56:54 +0800

    init pr

--- hw-base1/src/test/java/Test.java
+++ hw-base1/src/test/java/Test.java
@@ -1,9 +1,19 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class Test {
 
     @org.junit.Test
-    public void test() {
+    public void test() throws ParseException {
 
         System.out.println("hello word");
+
+        //2023/5/26
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        format.parse("2023/5/26");
+
+
 
     }
 
